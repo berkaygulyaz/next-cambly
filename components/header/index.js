6f1ec72b06a140d444debb4f76d5585388ab4d92
berkaygulyaz <berkaygulyaz@gmail.com>
2021-11-30 20:11:46 +0300

    Header construction started

--- components/header/index.js
+++ components/header/index.js
@@ -1,20 +1,46 @@
-import React from 'react';
-import Col from '../col';
-import ContainerFluid from '../container-fluid'
-import Row from '../row';
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import styles from "./style.module.scss";
+import Col from "../col";
+import ContainerFluid from "../container-fluid";
+import Row from "../row";
+import Logo from "../../src/assets/img/primary.png";
 
-function Header({ children }) {
-    return (
-      <header>
-          <ContainerFluid>
-              <Row>
-                  <Col>
-                    Berkay
-                  </Col>
-              </Row>
-          </ContainerFluid>
-      </header>
-    );
-  }
-  
-  export default Header;
+function Header({}) {
+  return (
+    <header className={styles.header}>
+      <ContainerFluid>
+        <Row>
+          <Col>
+            <div className={styles.headerWrapper}>
+              <Link href="/">
+                <a className="logo">
+                  <Image
+                    src={Logo}
+                    alt="Cambly Logo"
+                    width={140}
+                    height={40}
+                  />
+                </a>
+              </Link>
+              <nav className={styles.navbar}> 
+                <Link href="/tutors">
+                  <a>TUTORS</a>
+                </Link>
+                <Link href="/courses">
+                  <a>COURSES</a>
+                </Link>
+                <Link href="/history">
+                  <a>PROGRESS</a>
+                </Link>
+              </nav>
+            </div>
+          </Col>
+        </Row>
+      </ContainerFluid>
+    </header>
+  );
+}
+
+export default Header;
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
 import Head from 'next/head'
 import Header from '../components/header'
+import Layout from '../components/layout'
 export default function Home() {
   return (
     <div>
-      <Header />
+      <Layout>
+        
+      </Layout>
     </div>
   )
 }
