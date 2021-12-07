21c5b366e7033c580d5302cb8799e581ef9262e9
berkaygulyaz <berkaygulyaz@gmail.com>
2021-12-07 13:17:36 +0300

    Calendar info compoent is done

--- components/calendar-info/index.js
+++ components/calendar-info/index.js
@@ -3,7 +3,7 @@ import styles from "./style.module.scss";
 import Image from "next/image";
 import Link from "next/link";
 import calendar from "../../src/assets/img/calendar.png";
-import teacher from "../../src/assets/img/5a76f28a66029b0026708224s200.png"
+import teacher from "../../src/assets/img/5a76f28a66029b0026708224s200.png";
 function CalendarInfo() {
   const [show, setShow] = useState(false);
 
@@ -16,7 +16,12 @@ function CalendarInfo() {
         <Image src={calendar} alt="bell icon" width={20} height={20} />
       </div>
 
-      <div className={styles.calendarInfo}>
+      <div
+        className={styles.calendarInfo}
+        style={{
+          display: show ? "flex" : "none",
+        }}
+      >
         <div className={styles.menuTitle}>
           <p>Upcoming Lessons</p>
           <Link href="/">
@@ -30,9 +35,42 @@ function CalendarInfo() {
             <small>Wed</small>
           </h3>
           <div className={styles.lesson}>
-            <Image src={teacher} alt="arrow bottom" height={40} width={40} />
+            <Image
+              src={teacher}
+              className={styles.image}
+              alt="arrow bottom"
+              height={40}
+              width={40}
+            />
+            <div className={styles.teacherInfo}>
+              <span>Tomas L</span>
+              <p>5:00 PM - 5:30 PM</p>
+            </div>
           </div>
         </div>
+        <div className={styles.lessonInfo}>
+          <h3 className={styles.lessonDate}>
+            8
+            <br />
+            <small>Wed</small>
+          </h3>
+          <div className={styles.lesson}>
+            <Image
+              src={teacher}
+              className={styles.image}
+              alt="arrow bottom"
+              height={40}
+              width={40}
+            />
+            <div className={styles.teacherInfo}>
+              <span>Tomas L</span>
+              <p>5:00 PM - 5:30 PM</p>
+            </div>
+          </div>
+        </div>
+        <Link href="/">
+          <a className={styles.scheduleBtn}>+ Schedule A Lesson</a>
+        </Link>
       </div>
     </div>
   );
