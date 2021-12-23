1de1695150335a1e7e60afac909938bf0d9c2a9f
berkaygulyaz <berkaygulyaz@gmail.com>
2021-12-23 21:26:40 +0300

    Change file name

--- pages/_document.js
+++ pages/_document.js
@@ -15,7 +15,7 @@ class MyDocument extends Document {
             <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
             <link href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&display=swap" rel="stylesheet" />
         </Head>
-        <body>
+        <body style={{margin: 0 + "px"}}>
           <Main />
           <NextScript />
         </body>
