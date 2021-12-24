8a7171b0d5b48d6d65df41c1f9c18d01949a292d
berkaygulyaz <berkaygulyaz@gmail.com>
2021-12-24 14:57:45 +0300

    Create json file

---
+++ src/constant/index.js
@@ -0,0 +1,59 @@
+const CamblyConstants = {
+  HEADER: [
+    {
+      id: 1,
+      slug: "/#/tutors",
+      value: "Tutors",
+    },
+    {
+      id: 2,
+      slug: "/#/courses",
+      value: "Courses",
+    },
+    {
+      id: 3,
+      slug: "/#/progress",
+      value: "Progress",
+    },
+  ],
+
+  TUTORS: [
+    {
+      id: 1,
+      slug: "/#/tutors/Tomas-L",
+      name: "Tomas L.",
+      video:
+        "https://d3um8n5v1w9jhj.cloudfront.net/15718351/8344a551-0aad-4c77-a4e6-a4cd9534b7e2/archive.mp4?Expires=1640346661&Signature=Ha1llI3ODdUgXgA~RCbh0AJNYtnxRo1esfwamh9ngWLQ6T3f7ecxtzE5PLsKO1Aj9nlltSS8EpkvZLHyq3pHkD8mfIb8f567rOXsB76SVGTlvVvozxGs43lKjXeweqNPGRHwgdRjokh0mo52l1qKFIHYGTDKJQFco9qZqY~KYxPTJqA-rbu-PmZ3rkWMcvllujKW0RtspruyBhaA4qPuLulWAzvYqk~QKY7t05UGVTUJ-WFHLPv4OEQVdeq17wk0tObW5WbrWZa4lgbOYLCC7GnAGcL5UYXqmO~f97Xh6dNTEpp1DF7kVolH~cDZmP0efLmDmxwSmmxxx-z5Q0WCcQ__&Key-Pair-Id=APKAIKYVFETQQHXNXQ5Q",
+      avatar:
+        "https://camblyavatars.s3.amazonaws.com/5a76f28a66029b0026708224s200?h=9ae964f976073f0d4017c40fafd7a963",
+      about: "sports, art, culture, ancient history, etc,",
+      languages: "English (Native: USA Accent)",
+    },
+    {
+      id: 2,
+      slug: "/#/tutors/Shaun-Foster",
+      name: "shaun foster",
+      video:
+        "https://d1z2nqdm9ph1g6.cloudfront.net/604d02f35821bcfc8e26effa/604d02f35821bcfc8e26effa?Expires=1640348014&Signature=V85NEp8KXfzuGCrXWSJrogMpOjQAzO5~HUtwcOxS~GCcxu8QdjMdsV1HffoITI25QLFfyjDjQ56nZ1XKuXZjmdRgykXMfR7j4KXX1ZooHuW4FahEAw1MTXXpTG1g2No2o1EXdsflPC8Fav1~fDFz2IY~7i6JbfukPSe8UfiPKtezRDBDrbYe35neUqPPNKaiq64LkTN1q4QK1VZyGm2657e~otbfurdg1nUwJFdJP9n9dPWZQ-PYp89JGdUgKj-s-Ty-n5oXDOBX8vbdLIyX4bojM3RaiYdriLQeB~pRTXswaEYO3oXqArXhkxGHFOKWlzypz5aaOnetVRqSLUT-hw__&Key-Pair-Id=APKAIKYVFETQQHXNXQ5Q",
+      avatar:
+        "https://camblyavatars.s3.amazonaws.com/5cb4f0e997971b0024f98831s200",
+      about:
+        "I love running, music, traveling, and spending time with family and friends.",
+      languages: "English (Native: USA Accent)",
+    },
+    {
+      id: 3,
+      slug: "/#/tutors/Orlando-G",
+      name: "Orlando G",
+      video:
+        "https://d1z2nqdm9ph1g6.cloudfront.net/5e17ec68f0947edbb16370f1/5e17ec68f0947edbb16370f1?Expires=1640348116&Signature=Trxif3VxM03dQyZ6M1Vh-BPbvTytkZeKFthvQKGwtnjC0901AheulutVwyaWs6WM~9qOLbfWbhhwcuA3RwnsZevIrS47s3BzAbaJ4EJefrHphafeiWmdaA2Ju80ThFvH-e2SZtj~Hld29yWU9Jd7-Fs5gES9r2mhnL2~k61gcjkuSqdKoS0Mj8Y8cA-EUVER5LWCL~eUeShZ6LZBDof3PiUtxNIY24~hYc39prKYKeuQR9TMGKDlMVu8~gcxJyUPc~qC-HDJRGgIPeKHOwLFoTbY3HvPK9g7hRmiyh76AjoFRilTx3EmkSjlJVapHftkYEnjVMRPfq7iz6pJAd6McA__&Key-Pair-Id=APKAIKYVFETQQHXNXQ5Q",
+      avatar:
+        "https://camblyavatars.s3.amazonaws.com/5dd2f1c88d76fe0aca4a6a77s200?h=366f50aca9f138d27f74edd420be0506",
+      about:
+        "I am always interested in new technology overall, I enjoy playing soccer, lisening to music, going to the beach, video games and many more.",
+      languages: "English (Native: North American Accent)",
+    },
+  ],
+};
+
+export default QualityConstants;
