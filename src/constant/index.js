367784effc4abbdd11f22d1dde22b1dea2c92cf7
berkaygulyaz <berkaygulyaz@gmail.com>
2022-02-03 23:58:37 +0300

    Added new tutor

--- src/constant/index.js
+++ src/constant/index.js
@@ -132,6 +132,185 @@ const CamblyConstants = {
         "Intermediate",
       ],
     },
+    {
+      id: 4,
+      slug: "Oscar-R",
+      name: "Oscar R",
+      badge: "https://www.cambly.com/static/images/country-flag-icons/US.png",
+      location: "Monterrey, Mexico",
+      info:
+        "I am a graphic designer that found a passion for teaching the English language. I mostly have experience with business English.",
+      video:
+        "https://d1z2nqdm9ph1g6.cloudfront.net/5e17ec68f0947edbb16370f1/5e17ec68f0947edbb16370f1?Expires=1643820310&Signature=f3x~WvDk1VpITuZWxrTRI0wnzmDoU1k04tGk5ZLL6l4X0y9yTCFh-DQbCVDPx-WZzO3lenpvBfbfE5TWtDVBbvI16TsKdd0wcdN0bnvhD-VevTQIbIjwj7ooPrIORbELNJZA0kcuME6xfaHX8KjOWKcdh0d8jTzzW158F8cBYS9eS6FxlBoIbcDHfrPgX4Db-gGT-W0TRRBo-qRCpvnlGY~tpjOGLgnkatT~7zD~N~BsVyjfIaWy0YMk0IZVWDl32xDQU~jqEm6DJX8iUvTqubuhDdMFPlnOH1cId6qLuGQZ2T6GWLTZaFz2RWF5IWn-uRPxLKHP9voyo7l7FeJc1w__&Key-Pair-Id=APKAIKYVFETQQHXNXQ5Q",
+      avatar:
+        "https://camblyavatars.s3.amazonaws.com/5e01cb69da64af3c01e9b0dbs200",
+      about:
+        "I love the outdoors, going to the movies, concerts, shows and just try new things in general. I'm a certified food lover",
+      languages: "English (Native: USA Accent)",
+      chats: "1,000+ chats",
+      personalInfo: [
+        "North American Accent",
+        "Kind and Patient",
+        "Fun and Gregarious",
+        "Scholarly and Knowledgeable",
+        "Beginner",
+        "Intermediate",
+      ],
+    },
+    {
+      id: 5,
+      slug: "Meral-B",
+      name: "Meral B",
+      badge: "https://www.cambly.com/static/images/country-flag-icons/GB.png",
+      location: "Muğla",
+      info:
+        "Hello! My name is Meral. I'm half British & half Turkish. I'm a qualified English Language Teacher with lots of experience. I look forward to speaking to you very soon! ... oh by the way if you want to free talk, don't forget to bring something to talk about :)",
+      video:
+        "https://d1z2nqdm9ph1g6.cloudfront.net/5e17ec68f0947edbb16370f1/5e17ec68f0947edbb16370f1?Expires=1643820310&Signature=f3x~WvDk1VpITuZWxrTRI0wnzmDoU1k04tGk5ZLL6l4X0y9yTCFh-DQbCVDPx-WZzO3lenpvBfbfE5TWtDVBbvI16TsKdd0wcdN0bnvhD-VevTQIbIjwj7ooPrIORbELNJZA0kcuME6xfaHX8KjOWKcdh0d8jTzzW158F8cBYS9eS6FxlBoIbcDHfrPgX4Db-gGT-W0TRRBo-qRCpvnlGY~tpjOGLgnkatT~7zD~N~BsVyjfIaWy0YMk0IZVWDl32xDQU~jqEm6DJX8iUvTqubuhDdMFPlnOH1cId6qLuGQZ2T6GWLTZaFz2RWF5IWn-uRPxLKHP9voyo7l7FeJc1w__&Key-Pair-Id=APKAIKYVFETQQHXNXQ5Q",
+      avatar:
+        "https://camblyavatars.s3.amazonaws.com/5ea419d38a67b0a8e91c38a0s200",
+      about:
+        "I love the outdoors, going to the movies, concerts, shows and just try new things in general. I'm a certified food lover",
+      languages: "English (Native: British Accent)",
+      chats: "1,000+ chats",
+      teacher: "Teaching certificate ",
+      personalInfo: [
+        "North American Accent",
+        "Kind and Patient",
+        "Fun and Gregarious",
+        "Scholarly and Knowledgeable",
+        "Beginner",
+        "Intermediate",
+      ],
+    },
+    {
+      id: 6,
+      slug: "Teacher-Nelson",
+      name: "Teacher Nelson",
+      badge: "https://www.cambly.com/static/images/country-flag-icons/ZA.png",
+      location: "South Africa",
+      info:
+        "I am a CELTA qualified and experienced ESL professional teacher with great computing skills. Specialized in business and general English ed.",
+      video:
+        "https://d1z2nqdm9ph1g6.cloudfront.net/5e17ec68f0947edbb16370f1/5e17ec68f0947edbb16370f1?Expires=1643820310&Signature=f3x~WvDk1VpITuZWxrTRI0wnzmDoU1k04tGk5ZLL6l4X0y9yTCFh-DQbCVDPx-WZzO3lenpvBfbfE5TWtDVBbvI16TsKdd0wcdN0bnvhD-VevTQIbIjwj7ooPrIORbELNJZA0kcuME6xfaHX8KjOWKcdh0d8jTzzW158F8cBYS9eS6FxlBoIbcDHfrPgX4Db-gGT-W0TRRBo-qRCpvnlGY~tpjOGLgnkatT~7zD~N~BsVyjfIaWy0YMk0IZVWDl32xDQU~jqEm6DJX8iUvTqubuhDdMFPlnOH1cId6qLuGQZ2T6GWLTZaFz2RWF5IWn-uRPxLKHP9voyo7l7FeJc1w__&Key-Pair-Id=APKAIKYVFETQQHXNXQ5Q",
+      avatar:
+        "https://camblyavatars.s3.amazonaws.com/5f66501d5c258a656420317ds200",
+      about:
+        "Skydiving, cart racing, quad and mountain biking are my most favorite leisure activities; especially during the summer.",
+      languages: "English (Native: South African Accent)",
+      chats: "1,000+ chats",
+      teacher: "Teaching certificate ",
+      personalInfo: [
+        "North American Accent",
+        "Kind and Patient",
+        "Fun and Gregarious",
+        "Scholarly and Knowledgeable",
+        "Beginner",
+        "Intermediate",
+      ],
+    },
+    {
+      id: 7,
+      slug: "Estella-STJ",
+      name: "Estella STJ",
+      badge: "https://www.cambly.com/static/images/country-flag-icons/CA.png",
+      location: "Toronto, Canada",
+      info:
+        "Hi There, Let's chat! Current news, a Cambly lesson, or another topic that interests you....I'm a TESOL Communicative Approach teacher/tutor in the Toronto EST timezone. A success for me is when your session is enjoyable!",
+      video:
+        "https://d1z2nqdm9ph1g6.cloudfront.net/5e17ec68f0947edbb16370f1/5e17ec68f0947edbb16370f1?Expires=1643820310&Signature=f3x~WvDk1VpITuZWxrTRI0wnzmDoU1k04tGk5ZLL6l4X0y9yTCFh-DQbCVDPx-WZzO3lenpvBfbfE5TWtDVBbvI16TsKdd0wcdN0bnvhD-VevTQIbIjwj7ooPrIORbELNJZA0kcuME6xfaHX8KjOWKcdh0d8jTzzW158F8cBYS9eS6FxlBoIbcDHfrPgX4Db-gGT-W0TRRBo-qRCpvnlGY~tpjOGLgnkatT~7zD~N~BsVyjfIaWy0YMk0IZVWDl32xDQU~jqEm6DJX8iUvTqubuhDdMFPlnOH1cId6qLuGQZ2T6GWLTZaFz2RWF5IWn-uRPxLKHP9voyo7l7FeJc1w__&Key-Pair-Id=APKAIKYVFETQQHXNXQ5Q",
+      avatar:
+        "https://camblyavatars.s3.amazonaws.com/5ece9bfd57f556ef02652ba1s200?h=8fb07cb1092658450a63e682ed548f29",
+      about:
+        "I am a new TESOL teacher---with lots to share. In Toronto, I've been a homestay host for male and female international students from Japan, Kazakhstan, Russia, Turkey, Brazil, France, Saudi Arabia, and China: teens & young adults. I have a -4yr-BA-French Specialist and an MBA, Marketing & Entrepreneurial Studies focus. I've talked with many people during my work in banking, market research & fund...",
+      languages: "English (Native: Canadian Accent)",
+      chats: "1,000+ chats",
+      teacher: "Teaching certificate ",
+      personalInfo: [
+        "North American Accent",
+        "Kind and Patient",
+        "Fun and Gregarious",
+        "Scholarly and Knowledgeable",
+        "Beginner",
+        "Intermediate",
+      ],
+    },
+    {
+      id: 8,
+      slug: "Tex",
+      name: "Tex",
+      badge: "https://www.cambly.com/static/images/country-flag-icons/US.png",
+      location: "Texas, USA",
+      info:
+        "I’m happy to work with students of all ages. I love helping them achieve their goals. I look forward to working with you!",
+      video:
+        "https://d1z2nqdm9ph1g6.cloudfront.net/5e17ec68f0947edbb16370f1/5e17ec68f0947edbb16370f1?Expires=1643820310&Signature=f3x~WvDk1VpITuZWxrTRI0wnzmDoU1k04tGk5ZLL6l4X0y9yTCFh-DQbCVDPx-WZzO3lenpvBfbfE5TWtDVBbvI16TsKdd0wcdN0bnvhD-VevTQIbIjwj7ooPrIORbELNJZA0kcuME6xfaHX8KjOWKcdh0d8jTzzW158F8cBYS9eS6FxlBoIbcDHfrPgX4Db-gGT-W0TRRBo-qRCpvnlGY~tpjOGLgnkatT~7zD~N~BsVyjfIaWy0YMk0IZVWDl32xDQU~jqEm6DJX8iUvTqubuhDdMFPlnOH1cId6qLuGQZ2T6GWLTZaFz2RWF5IWn-uRPxLKHP9voyo7l7FeJc1w__&Key-Pair-Id=APKAIKYVFETQQHXNXQ5Q",
+      avatar:
+        "https://camblyavatars.s3.amazonaws.com/60431148f631c2fc30187530s200",
+      about:
+        "I am a native Texan, but I've lived in other states in the Southwestern United States. I like to laugh and make others laugh too! I enjoy crafting projects, DIY, volunteering, American football, Netflix, music, reading, fashion, and interior design. In the past, I've worked as a CPA/Tax administrative assistant, a Medical office specialist, and in customer service/retail as a small business owner. I’ve also done volunteer work for many years.",
+      languages: "English (Native: USA Accent)",
+      personalInfo: [
+        "North American Accent",
+        "Kind and Patient",
+        "Fun and Gregarious",
+        "Scholarly and Knowledgeable",
+        "Beginner",
+        "Intermediate",
+      ],
+    },
+    {
+      id: 9,
+      slug: "David-Timmerman",
+      name: "David Timmerman",
+      badge: "https://www.cambly.com/static/images/country-flag-icons/US.png",
+      location: "Myrtle Beach, SC",
+      info:
+        "I can help you improve your English speaking, reading and writing skills. I'm knowledgeable, patient, speak slowly, and pronounce clearly.",
+      video:
+        "https://d1z2nqdm9ph1g6.cloudfront.net/5e17ec68f0947edbb16370f1/5e17ec68f0947edbb16370f1?Expires=1643820310&Signature=f3x~WvDk1VpITuZWxrTRI0wnzmDoU1k04tGk5ZLL6l4X0y9yTCFh-DQbCVDPx-WZzO3lenpvBfbfE5TWtDVBbvI16TsKdd0wcdN0bnvhD-VevTQIbIjwj7ooPrIORbELNJZA0kcuME6xfaHX8KjOWKcdh0d8jTzzW158F8cBYS9eS6FxlBoIbcDHfrPgX4Db-gGT-W0TRRBo-qRCpvnlGY~tpjOGLgnkatT~7zD~N~BsVyjfIaWy0YMk0IZVWDl32xDQU~jqEm6DJX8iUvTqubuhDdMFPlnOH1cId6qLuGQZ2T6GWLTZaFz2RWF5IWn-uRPxLKHP9voyo7l7FeJc1w__&Key-Pair-Id=APKAIKYVFETQQHXNXQ5Q",
+      avatar:
+        "https://camblyavatars.s3.amazonaws.com/5fc9872325bad785f8b03d41s200",
+      about:
+        "Love teaching English, meeting people from different countries and cultures, traveling, foreign languages, personal fitness training, hiking, music. Have spent at least 6 months in 5 countries and visited nearly 30 countries.",
+      languages: "English (Native: USA Accent)",
+      chats: "1,000+ chats",
+      teacher: "Teaching certificate ",
+      personalInfo: [
+        "North American Accent",
+        "Kind and Patient",
+        "Fun and Gregarious",
+        "Scholarly and Knowledgeable",
+        "Beginner",
+        "Intermediate",
+      ],
+    },
+    {
+      id: 10,
+      slug: "EnglishWithMolly",
+      name: "EnglishWithMolly",
+      badge: "https://www.cambly.com/static/images/country-flag-icons/GB.png",
+      location: "United Kingdom",
+      info:
+        "I have lived in several countries and know how amazing it is to learn a new language. I look forward to helping my students grow.",
+      video:
+        "https://d1z2nqdm9ph1g6.cloudfront.net/5e17ec68f0947edbb16370f1/5e17ec68f0947edbb16370f1?Expires=1643820310&Signature=f3x~WvDk1VpITuZWxrTRI0wnzmDoU1k04tGk5ZLL6l4X0y9yTCFh-DQbCVDPx-WZzO3lenpvBfbfE5TWtDVBbvI16TsKdd0wcdN0bnvhD-VevTQIbIjwj7ooPrIORbELNJZA0kcuME6xfaHX8KjOWKcdh0d8jTzzW158F8cBYS9eS6FxlBoIbcDHfrPgX4Db-gGT-W0TRRBo-qRCpvnlGY~tpjOGLgnkatT~7zD~N~BsVyjfIaWy0YMk0IZVWDl32xDQU~jqEm6DJX8iUvTqubuhDdMFPlnOH1cId6qLuGQZ2T6GWLTZaFz2RWF5IWn-uRPxLKHP9voyo7l7FeJc1w__&Key-Pair-Id=APKAIKYVFETQQHXNXQ5Q",
+      avatar:
+        "https://camblyavatars.s3.amazonaws.com/5fc96e083dfa71da28b92afcs200",
+      about:
+        "Since a young age I have had a passion for languages. I have been speaking Spanish since I was 6 years old and have a passion for learning other languages. I have had great opportunities to live abroad, including Cyprus and Barcelona. I love to learn more about people and their cultures.",
+      languages: "English (Native: British Accent)",
+      chats: "500+ chats",
+      teacher: "Teaching certificate ",
+      personalInfo: [
+        "North American Accent",
+        "Kind and Patient",
+        "Fun and Gregarious",
+        "Scholarly and Knowledgeable",
+        "Beginner",
+        "Intermediate",
+      ],
+    },
   ],
 
   FILTER: [
